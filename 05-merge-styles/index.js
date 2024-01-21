1a4a7c684edbc23c086c0808c38c865eb2cd70f7
Raich94 <raichxxx@mail.ru>
2024-01-21 20:39:27 +0300

    refactor: delete comments in task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 const bundle = path.resolve(__dirname, 'project-dist', 'bundle.css');
 
-// fs.appendFile(bundle, '',(err) => {
-//     if (err) throw err;
-// });
 let data = '';
 
 fs.readdir(path.resolve(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
@@ -24,24 +21,3 @@ fs.readdir(path.resolve(__dirname, 'styles'), { withFileTypes: true }, (err, fil
         })
     }
 })
-
-// const xz = async () => {
-//     fs.readdir(path.resolve(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
-//         if (err)
-//             console.log(err);
-//         else {
-//             files.forEach(file => {
-//                 if (path.extname(file.name) === '.css') {
-//                     const readSrteam = fs.createReadStream(path.resolve(__dirname, 'styles', file.name), 'utf-8');
-//                     const writeStream = fs.createWriteStream(bundle);
-//                     readSrteam.forEach(chunk => {
-//                         data += chunk +'\n';
-//                         writeStream.write(data)
-//                     })
-//                 }
-//             })
-//         }
-//     })
-// }
-
-// xz()
